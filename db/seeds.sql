606dec6cc31920f4490933cc0021f2664ea85db8
aeryk <aerykmarrero@yahoo.com>
2020-11-10 07:38:34 -0600

    redoing burger build

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bigger Mac", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Slopper", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Heart Attack Snack", true);
